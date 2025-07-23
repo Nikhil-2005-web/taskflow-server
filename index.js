a537054692292da23fc9ed4150cfcafdbfe812ef
Nikhil-2005-web <nikhilgurram10001@gmail.com>
2025-07-23 14:29:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const taskRoutes = require("./routes/tasks");
+app.use("/api/tasks", taskRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server running on port", process.env.PORT);
+    });
+  })
+  .catch((error) => console.log(error.message));
